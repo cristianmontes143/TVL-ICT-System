1aaccd026953def8c8bc544c5ef45b7d141be990
cristianmontes143 <crischuychuyz@gmail.com>
2025-01-20 13:58:58 +0800

    home.html flash edit

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db   ##means from __init__.py import db
+from . import db  
 from flask_login import login_user, login_required, logout_user, current_user
 
 
@@ -17,7 +17,7 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+                flash('Your Account is Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
